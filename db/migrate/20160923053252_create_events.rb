97b96a0e02d5e3d22381fa382103be09258891f7
Jeremy Perez <glyde06@hotmail.com>
2016-09-23 23:30:17 -0500

    created and ran the events migration.

--- db/migrate/20160923053252_create_events.rb
+++ db/migrate/20160923053252_create_events.rb
@@ -1,7 +1,20 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
-
+	    t.string :name
+	    t.text :description
+	    t.binary :image
+	    t.string :phone_number
+	    t.string :directions_url
+	    t.string :location
+	    t.string :address
+	    t.string :city
+	    t.string :state
+	    t.string :zip_code
+	    t.date :end_date
+	    t.date :start_date
+    	t.time :start_time
+    	t.time :end_time
       t.timestamps
     end
   end
