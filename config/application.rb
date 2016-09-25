c0c0194b0333d172beee708e87ef59d70e6e38a3
Jeremy Perez <glyde06@hotmail.com>
2016-09-25 14:24:20 -0500

    changing these things back since I don't believe that actually effected me being able to deploy. The deployment worked just fine once I fixed the type-o in the bootstrap css file in the previous commit.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module WestEndChurch
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # config.serve_static_assets = true
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
   end
 end
