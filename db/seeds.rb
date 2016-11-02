094d2f102654d005fefea15903d90f8510c28f1a
Jeremy Perez <glyde06@hotmail.com>
2016-11-02 00:56:41 -0500

    seed queries to create some events in the db for testing purposes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create!(name: "Test Event 1", description: "THis is just a test guys...", phone_number: "972-780-7654", directions_url: "https://www.google.com/maps/place/915+San+Jacinto+St,+Dallas,+TX+75202/@32.781965,-96.8065507,17z", location: "West End Church", address: "915 San Jacinto St", city: "Dallas", state: "TX", zip_code: "75202", start_date: "2016-11-13", end_date: "2016-11-13", start_time: "2016-11-13 11:00:00", end_time: "2016-11-13 13:00:00")
+Event.create!(name: "Test Event 2", description: "Like I said last time. THis is just a test guysssss...", phone_number: "972-780-7654", directions_url: "https://www.google.com/maps/place/915+San+Jacinto+St,+Dallas,+TX+75202/@32.781965,-96.8065507,17z", location: "West End Church", address: "915 San Jacinto St", city: "Dallas", state: "TX", zip_code: "75202", start_date: "2016-11-20", end_date: "2016-11-20", start_time: "2016-11-20 11:00:00", end_time: "2016-11-20 13:00:00")
