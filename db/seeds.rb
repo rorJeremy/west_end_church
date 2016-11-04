6a9fa603de42de81c42917bd998b434500e8b62e
Jeremy Perez <glyde06@hotmail.com>
2016-11-04 01:30:10 -0500

    I added a comment in the seeds file that is the commands I used to add an image from my local Desktop to the database using activerecord.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 
 Event.create!(name: "Test Event 1", description: "THis is just a test guys...", phone_number: "972-780-7654", directions_url: "https://www.google.com/maps/place/915+San+Jacinto+St,+Dallas,+TX+75202/@32.781965,-96.8065507,17z", location: "West End Church", address: "915 San Jacinto St", city: "Dallas", state: "TX", zip_code: "75202", start_date: "2016-11-13", end_date: "2016-11-13", start_time: "2016-11-13 11:00:00", end_time: "2016-11-13 13:00:00")
 Event.create!(name: "Test Event 2", description: "Like I said last time. THis is just a test guysssss...", phone_number: "972-780-7654", directions_url: "https://www.google.com/maps/place/915+San+Jacinto+St,+Dallas,+TX+75202/@32.781965,-96.8065507,17z", location: "West End Church", address: "915 San Jacinto St", city: "Dallas", state: "TX", zip_code: "75202", start_date: "2016-11-20", end_date: "2016-11-20", start_time: "2016-11-20 11:00:00", end_time: "2016-11-20 13:00:00")
+
+# west_end_church_picture_local = File.open("../Desktop/west_end_church.jpg", 'rb').read
+# Event.first.update_attributes!(image: west_end_church_picture_local)
