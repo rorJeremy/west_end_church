cc5979bc9de05918f1e871831272c81ac7f4a6be
Jeremy Perez <glyde06@hotmail.com>
2016-11-06 00:20:28 -0500

    just a little error handling logic for whenever there isn't any upcoming events.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,13 @@ class HomeController < ApplicationController
 		@events = Event.all.sort_by &:start_date
 		@upcoming_event = @events.shift
 
-
-		upcoming_event_date = @upcoming_event.start_date
-		countdown = Time.now.to_date.distance_to(upcoming_event_date)
-		@days_left = countdown[:days]
-		@months_left = countdown[:months]
-		@years_left = countdown[:years]
+		unless @upcoming_event.nil?
+			upcoming_event_date = @upcoming_event.start_date
+			countdown = Time.now.to_date.distance_to(upcoming_event_date)
+			@days_left = countdown[:days]
+			@months_left = countdown[:months]
+			@years_left = countdown[:years]
+		end
 
 	end
 	
