2c225590ed35b71c3c382cb7340c891f21360839
Jeremy Perez <glyde06@hotmail.com>
2016-11-06 22:04:06 -0600

    added the api routes and controller that I generated with this command: rails g controller api/v1/events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :events
+	namespace :api do
+		namespace :v1 do
+	 		resources :events, :defaults => { :format => 'json' }
+	 	end
+	end  
   get '/events/:id/image' => 'events#image', as: :event_image
   get 'home', to: 'home#home'
   get 'contact', to: 'home#contact'
