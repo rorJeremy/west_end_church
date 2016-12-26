f1d005ab727d750e4b3e288172b97245d20dc06d
Jeremy Perez <glyde06@hotmail.com>
2016-12-26 16:38:17 -0600

    updated the photo gallery migration and then ran the db:migrate.

--- db/migrate/20161226223134_create_photo_galleries.rb
+++ db/migrate/20161226223134_create_photo_galleries.rb
@@ -1,7 +1,9 @@
 class CreatePhotoGalleries < ActiveRecord::Migration[5.0]
   def change
     create_table :photo_galleries do |t|
-
+	    t.string :title
+	    t.boolean :active
+	    t.binary :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923053252) do
+ActiveRecord::Schema.define(version: 20161226223134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20160923053252) do
     t.time     "end_time"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "photo_galleries", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "active"
+    t.binary   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
